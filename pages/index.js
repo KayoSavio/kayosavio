a1a35e6283d1d7997579db7fbed4b136d0558e57
Kayo <kayosavio@hotmail.com>
2020-11-19 02:08:50 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function IndexPage() {
       <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
+          Kayo Savio
         </h1>
       </div>
     </div>
