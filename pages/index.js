22f7e77b14215e859068986c10f31703101f97e7
Kayo <kayosavio@hotmail.com>
2020-11-29 19:30:07 -0300

    em breve novidades

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,7 @@ export default function IndexPage() {
     <div>
       <Nav />
       <div className="bg-white py-20">
-        <h1 className="text-6xl  text-center text-black">
-          Loading ...
-        </h1>
+        
         <h1 className="text-5xl color-fff text-center text-gray-500">
           Em Breve Novidades
                 </h1>
